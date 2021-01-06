98de10e0275ff197cdd860a6d7fd3da02459514b
panther <jackfunck21@gmail.com>
2021-01-06 15:08:32 -0500

    working on save button and trash button

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-//IVEN I am using a daily planner to create a schedule
 
-
-//WHEN I open the planner
-//THEN the current day is displayed at the top of the calendar
-
-
-//WHEN I scroll down
-//THEN I am presented with time blocks for standard business hours
-
-//WHEN I view the time blocks for that day
-//THEN each time block is color-coded to indicate whether it is in the past, present, or future
-//WHEN I click into a time block
-//THEN I can enter an event
-//WHEN I click the save button for that time block
-//THEN the text for that event is saved in local storage
-//WHEN I refresh the page
-//THEN the saved events persist
-
-// current date displayed at top
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 //each time block is color-coded to indicate weather it is in the past.
@@ -26,10 +7,10 @@ var d = new Date();
 var n = d.getHours();
 $(".hour").each(function(){
     var currentTIme = $(this).attr("data-time");
-    console.log("current time: "+currentTIme);
-    console.log("hour: "+n);
-    console.log(typeof currentTIme);
-    console.log(typeof n);
+    //console.log("current time: "+currentTIme);
+    //console.log("hour: "+n);
+    //console.log(typeof currentTIme);
+    //console.log(typeof n);
 
     if(currentTIme < n)
     {
@@ -52,14 +33,20 @@ timeColor();
 
 // save button function to local storage even when refreshing
 function saveEvent (event){
+var theInput = $(this).sibling("input").value.trim();
 
-    
+window.localStorage.setItem(theInput);
 }
+
+$(".saveBtn").on("click", saveEvent);
+
+
 // Trash delete button function
+function destroy (event){
+ var trash = $(this).siblings("input");
 
+ localStorage.
 
+ 
 
-
-
-
-
+}
